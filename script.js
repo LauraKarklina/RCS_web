7ea1b41afb58d2b3b34384508df7b0baa4d03429
LauraK <laura.karklina.lk@gmail.com>
2020-09-30 15:58:09 +0300

    counter

--- script.js
+++ script.js
@@ -15,23 +15,46 @@ console.log(x);
 
 console.log(output); /*šis f12 Consolē parāda darbības*/
 
-addButton(0) /*fukcija sāksies ar norādīto ciparu-0*/
+addButton(1) /*fukcija sāksies ar norādīto ciparu-0*/
+let lastNumber=42; /*norāda pēdējo ciparu, lai var ar pogu pievienot nākošo*/
+
+
 
 function addButton(i){
     let output=document.getElementById("output");/*definē jaunu mainīgo, kuru paņem no html faila*/
     let button=document.createElement("button");/*izveido pašu pogu šajā failā, kuru nosaucam par button*/
-    button.style.background="green"
     
-    i=i+1; 
-    button.textContent = "button"+i; /*izveido pogai nosaukumu un klāt liek n-to reizi klāt cik funkcija atkārtojas*/
-    output.append(button);/*šis pievieno pogu(šīs lapas outputu) html kodam, kur ir button*/
-   
-    if(i<42){
-        if(i%3){
-            button.style.background="white"
-        }
-        
-        addButton(i); /*šis fikciju atkārto sevi tik reizes, kamēr i>42*/
+    
+    if(i%3==0){
+        button.style.background="green"
+        button.setAttribute("data-number", i)
+        button.addEventListener("click", function(){
+            let nextNumber=Number(button.getAttribute("data-number", i))+1;
+            button.setAttribute("data-number", nextNumber); 
+            button.textContent = "button"+nextNumber
+        });
         
     }
+   
+    button.textContent = i; /*izveido pogai nosaukumu un klāt liek n-to reizi klāt cik funkcija atkārtojas*/
+    /*let counter=button.addEventListener("click", function(){
+        alert("Button was pressed");*/
+    output.append(button);/*šis pievieno pogu(šīs lapas outputu) html kodam, kur ir button*/
+    
+    i=i+1; 
+    if(i<=42){   
+        addButton(i); /*šis fikciju atkārto sevi tik reizes, kamēr i>42*/
+    }
+    
 }
+
+let add_button=document.getElementById("add");/*Paņemam no index faila "add" pogu. poga pievieno klāt jaunu ciparu*/
+add_button.addEventListener("click", function(){
+    lastNumber=lastNumber+1;
+    addButton(lastNumber)
+    
+});
+
+
+    
+    
